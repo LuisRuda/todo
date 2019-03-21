c4f48b5613bdb594dc1eaa7b7fa0cf693971a760
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-21 20:53:04 -0300

    Componente para adicionar tarefa

---
+++ src/components/TodoForm.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { View, StyleSheet, Button } from 'react-native'
+
+import Input from './Input'
+
+export default class TodoForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state= {
+            text: ''
+        }
+    }
+
+    onChangeText(text) {
+        this.setState({
+            text
+        })
+    }
+    onPress() {
+        console.log(this.state.text)
+    }
+
+    render() {
+        const { text } = this.state
+        return(
+            <View style={styles.formContainer}>
+                <View style={styles.inputContainer}>
+                    <Input
+                        onChangeText={text => this.onChangeText(text)}
+                        value={text} />
+                </View>
+                <View>
+                    <Button 
+                        style={styles.buttonContainer}
+                        onPress={() => this.onPress()} 
+                        title='Add' />
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    formContainer: {
+        flexDirection: 'row'
+    },
+    inputContainer: {
+        flex: 3
+    },
+    buttonContainer: {
+        flex: 1
+    }
+})
